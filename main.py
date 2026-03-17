67ca91808a7c7ba2c88df18527cf5d261024d8a0
efraimmarinho <efraimzikka10@gmail.com>
2026-03-17 15:18:13 -0300

    Adiciona logica de calculos no main.py

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from processamento import calcular_media, definir_situacao, gerar_relatorio_txt
 
 def main():
+    dados_alunos = []
+    
     print(" SISTEMA DE NOTAS SENAI ")
-    pass # O pass serve só pra função não ficar vazia e dar erro agora
 
+    if len(dados_alunos) > 0:
+        resultados = []
+        maior_media = -1.0
+        aluno_destaque = {"nome": "", "media": 0.0}
+        
+        print("\nProcessando tudo, aguarde...")
+        
+        for tupla_aluno in dados_alunos:
+            nome_aluno = tupla_aluno[0]
+            notas_aluno = tupla_aluno[1]
+            
+            media = calcular_media(notas_aluno)
+            situacao = definir_situacao(media)
+            
+            resultados.append({
+                "nome": nome_aluno,
+                "media": media,
+                "situacao": situacao
+            })
+            
+            if media > maior_media:
+                maior_media = media
+                aluno_destaque["nome"] = nome_aluno
+                aluno_destaque["media"] = media
+        
+        gerar_relatorio_txt(resultados, aluno_destaque)
+        print("Finalizado! O arquivo 'resultado.txt' foi criado na pasta.")
+    else:
+        print("Nenhum dado válido foi inserido.")
+        
 if __name__ == "__main__":
     main()
